69dabf914a11c1bce158473061787e27638b8f3e
mikm7243 <mikm7243@naver.com>
2024-09-14 15:35:11 +0900

    init

---
+++ src/main/java/lems/cowshed/CowshedApplication.java
@@ -0,0 +1,13 @@
+package lems.cowshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowshedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowshedApplication.class, args);
+	}
+
+}
