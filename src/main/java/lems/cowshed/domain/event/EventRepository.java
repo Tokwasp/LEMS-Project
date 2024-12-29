f6c70ca8d089c02923e8201838648d91c3c322a2
syounng <stellaj0522@gmail.com>
2024-12-29 17:21:24 +0900

    refactor: add repository interface methods

--- src/main/java/lems/cowshed/domain/event/EventRepository.java
+++ src/main/java/lems/cowshed/domain/event/EventRepository.java
@@ -3,11 +3,17 @@ package lems.cowshed.domain.event;
 import lems.cowshed.api.controller.dto.event.response.EventPreviewResponseDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EventRepository {
     Slice<EventPreviewResponseDto> findAll(Long lastEventId, Pageable pageable);
+    Event findOneById(Long eventId);
+    List<Event> findAllByKeyword(String keyword);
+    List<Event> findAllOrderByCreatedDate();
+    List<Event> findAllOrderByApplicants();
+    List<Event> findAllByCategory(String category);
 //    void save(Event event);
 //    Optional<Event> findById(Long id);
 //    List<Event> findByCategory(Category category);
@@ -15,5 +21,4 @@ public interface EventRepository {
 //    List<Event> findByDistance();
 //    void delete(Event event);
 //    void edit(Event event);
-
 }
