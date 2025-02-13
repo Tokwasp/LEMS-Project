b311fb5b46b1fab7d5ded73d04947b2c6b1be3b1
mikm7243 <mikm7243@naver.com>
2025-02-14 00:48:18 +0900

    refactor: separate setting application and test

--- src/test/java/lems/cowshed/domain/user/UserRepositoryTest.java
+++ src/test/java/lems/cowshed/domain/user/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
