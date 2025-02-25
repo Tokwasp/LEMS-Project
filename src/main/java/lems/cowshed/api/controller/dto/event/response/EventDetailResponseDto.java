6830a6695c69a25acb19c4a3c491a879f59e3155
mikm7243 <mikm7243@naver.com>
2025-02-25 16:18:42 +0900

    refactor : delete event Address

--- src/main/java/lems/cowshed/api/controller/dto/event/response/EventDetailResponseDto.java
+++ src/main/java/lems/cowshed/api/controller/dto/event/response/EventDetailResponseDto.java
@@ -25,8 +25,6 @@ public class EventDetailResponseDto {
     Category category;
     @Schema(description = "등록일", example = "yyyy-mm-dd hh:mm:ss")
     LocalDateTime createdDate;
-    @Schema(description = "주소", example = "서울특별시 송파구 오금로55길")
-    String address;
     @Schema(description = "장소 이름", example = "스카이 휘트니스")
     String location;
     @Schema(description = "내용", example = "같이 운동하실 분 구합니다. 같이 프레스 운동 하면서 서로 보조해주실 분 구합니다.")
@@ -42,7 +40,7 @@ public class EventDetailResponseDto {
 
     @Builder
     private EventDetailResponseDto(Long eventId, String author, String name, Category category,
-                                  LocalDateTime createdDate, String address, String location,
+                                  LocalDateTime createdDate, String location,
                                   String content, LocalDate eventDate, int capacity, long applicants,
                                    BookmarkStatus bookmarkStatus) {
         this.eventId = eventId;
@@ -50,7 +48,6 @@ public class EventDetailResponseDto {
         this.author = author;
         this.category = category;
         this.createdDate = createdDate;
-        this.address = address;
         this.location = location;
         this.content = content;
         this.eventDate = eventDate;
@@ -66,7 +63,6 @@ public class EventDetailResponseDto {
                 .author(event.getAuthor())
                 .category(event.getCategory())
                 .createdDate(event.getCreatedDateTime())
-                .address(event.getAddress())
                 .location(event.getLocation())
                 .content(event.getContent())
                 .eventDate(event.getEventDate())
