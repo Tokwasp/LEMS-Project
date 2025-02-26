1f1e1c9cb03a984ac42b52976db6589dcdf0be15
mikm7243 <mikm7243@naver.com>
2025-02-26 13:27:36 +0900

    refactor : remove Not used query

--- src/main/java/lems/cowshed/domain/userevent/UserEventRepository.java
+++ src/main/java/lems/cowshed/domain/userevent/UserEventRepository.java
@@ -1,6 +1,5 @@
 package lems.cowshed.domain.userevent;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +12,6 @@ public interface UserEventRepository extends JpaRepository<UserEvent, Long> {
     @Query("SELECT COUNT(ue) From UserEvent ue Where ue.event.id = :eventId")
     long countParticipantByEventId(@Param("eventId") Long eventId);
 
-    @EntityGraph(attributePaths = {"user"})
-    List<UserEvent> findFetchUserByEventId (Long eventId);
     List<UserEvent> findByEventIdIn (List<Long> eventIds);
     Optional<UserEvent> findByEventIdAndUserId (Long EventId, Long userId);
 }
