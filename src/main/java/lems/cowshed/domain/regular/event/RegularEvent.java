c8faee5e2ab406f1c0302ba99f3de7401419ac36
mikm7243 <mikm7243@naver.com>
2025-05-04 18:33:21 +0900

    refactor: regularEvent change date To dateTime

--- src/main/java/lems/cowshed/domain/regular/event/RegularEvent.java
+++ src/main/java/lems/cowshed/domain/regular/event/RegularEvent.java
@@ -3,12 +3,16 @@ package lems.cowshed.domain.regular.event;
 import jakarta.persistence.*;
 import lems.cowshed.domain.BaseEntity;
 import lems.cowshed.domain.event.Event;
+import lems.cowshed.domain.regular.event.participation.RegularEventParticipation;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,7 +23,7 @@ public class RegularEvent extends BaseEntity {
     private Long id;
 
     private String name;
-    private LocalDate date;
+    private LocalDateTime dateTime;
     private String location;
     private int capacity;
 
@@ -27,20 +31,23 @@ public class RegularEvent extends BaseEntity {
     @JoinColumn(name = "event_id")
     private Event event;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RegularEventParticipation> participations = new ArrayList<>();
+
     @Builder
-    private RegularEvent(String name, LocalDate date, String location, int capacity, Event event) {
+    private RegularEvent(String name, LocalDateTime dateTime, String location, int capacity, Event event) {
         this.name = name;
-        this.date = date;
+        this.dateTime = dateTime;
         this.location = location;
         this.capacity = capacity;
         this.event = event;
     }
 
-    public static RegularEvent of(String name, LocalDate date, String location,
+    public static RegularEvent of(String name, LocalDateTime dateTime, String location,
                                   int capacity, Event event){
         return RegularEvent.builder()
                 .name(name)
-                .date(date)
+                .dateTime(dateTime)
                 .location(location)
                 .capacity(capacity)
                 .event(event)
@@ -49,5 +56,17 @@ public class RegularEvent extends BaseEntity {
 
     public boolean isNotPossibleParticipation(long participantCount) {
         return capacity <= participantCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RegularEvent that = (RegularEvent) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
