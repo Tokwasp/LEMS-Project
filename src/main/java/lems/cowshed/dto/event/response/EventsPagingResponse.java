f6e8c3d14c70b4fe556689bac8a8c2806b7d4bc3
mikm7243 <mikm7243@naver.com>
2025-06-01 19:47:23 +0900

    refactor: 응답 객체 이름 변경

--- src/main/java/lems/cowshed/dto/event/response/EventsPagingResponse.java
+++ src/main/java/lems/cowshed/dto/event/response/EventsPagingResponse.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 @Getter
 @Schema(description = "메인 페이지의 모임 리스트")
-public class EventsPagingInfo {
-    private List<EventSimpleInfo> content;
+public class EventsPagingResponse {
+    private List<EventPagingInfo> content;
     private boolean isLast;
 
     @Builder
-    public EventsPagingInfo(List<EventSimpleInfo> content, boolean isLast) {
+    public EventsPagingResponse(List<EventPagingInfo> content, boolean isLast) {
         this.content = content;
         this.isLast = isLast;
     }
 
-    public static EventsPagingInfo of(List<EventSimpleInfo> content, boolean isLast){
-        return EventsPagingInfo.builder()
+    public static EventsPagingResponse of(List<EventPagingInfo> content, boolean isLast){
+        return EventsPagingResponse.builder()
                 .content(content)
                 .isLast(isLast)
                 .build();
