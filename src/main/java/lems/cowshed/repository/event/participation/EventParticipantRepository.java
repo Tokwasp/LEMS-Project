7e0491aba87e2b2a68c16582777f55c4340179f8
Tokwasp <mikm7243@naver.com>
2025-08-12 13:26:46 +0900

    refactor: 모임 참여 서비스 로직 연관관계 제거로 인한 리팩토링

--- src/main/java/lems/cowshed/repository/event/participation/EventParticipantRepository.java
+++ src/main/java/lems/cowshed/repository/event/participation/EventParticipantRepository.java
@@ -10,14 +10,19 @@ import java.util.Optional;
 
 public interface EventParticipantRepository extends JpaRepository<EventParticipation, Long> {
 
-    @Query("SELECT COUNT(ep) From EventParticipation ep Where ep.event.id = :eventId")
+    @Query("SELECT COUNT(ep) From EventParticipation ep Where ep.eventId = :eventId")
     long getParticipationCountById(@Param("eventId") Long eventId);
 
     List<EventParticipation> findEventParticipationByEventIdIn(List<Long> eventIds);
 
     Optional<EventParticipation> findByEventIdAndUserId(Long eventId, Long userId);
 
+    @Query("select ep from EventParticipation ep where ep.eventId in :eventId")
+    List<EventParticipation> findByEventId(@Param("eventId") Long eventId);
+
+    @Query("select ep from EventParticipation ep where ep.eventId in :eventIds")
+    List<EventParticipation> findByEventIdIn(@Param("eventIds") List<Long> eventIds);
+
     @Query("SELECT count(ep) from EventParticipation ep where ep.user.id = :userId")
     long findCountByUserId(@Param("userId") Long userId);
-
 }
