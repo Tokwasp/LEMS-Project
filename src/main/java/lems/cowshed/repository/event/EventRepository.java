d069c61e0636d46968d04192566611a207679eee
Tokwasp <mikm7243@naver.com>
2025-08-12 13:51:57 +0900

    refactor: 연관관계 제거로 인한 모임 쿼리 수정

--- src/main/java/lems/cowshed/repository/event/EventRepository.java
+++ src/main/java/lems/cowshed/repository/event/EventRepository.java
@@ -17,7 +17,8 @@ import java.util.Set;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    Optional<Event> finByIdWithOptimisticLock(Long eventId);
+    @Query("select e from Event e where e.id = :eventId")
+    Optional<Event> finByIdWithOptimisticLock(@Param("eventId") Long eventId);
 
     Slice<Event> findEventsBy(Pageable pageable);
     Event findByName(String name);
@@ -27,8 +28,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT b.event.id FROM Bookmark b WHERE b.userId = :userId AND b.event.id IN :eventIds AND b.status = :bookmarkStatus")
     Set<Long> findEventIdsBookmarkedByUser(@Param("userId") Long userId, @Param("eventIds") List<Long> eventIds, @Param("bookmarkStatus") BookmarkStatus bookmarkStatus);
 
-    @Query("select e from Event e left join e.participants ep where e.id In :eventIds order by e.id desc")
-    List<Event> findByIdIn(List<Long> eventIds);
+    @Query("select e from Event e left join EventParticipation ep on e.id = ep.eventId where e.id In :eventIds order by e.id desc")
+    List<Event> findByIdIn(@Param("eventIds") List<Long> eventIds);
 
     @Query("select distinct e from Event e left join fetch e.bookmarks b where e.id In :eventIds")
     List<Event> findByIdInFetchBookmarks(List<Long> eventIds);
