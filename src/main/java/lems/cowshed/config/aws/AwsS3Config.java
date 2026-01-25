805c4cd3639f19af7b6ca1e75115c25cf7c215ad
johuije <mikm7243@naver.com>
2026-01-25 13:13:24 +0900

    feat: aws sdk v2 버전으로 S3 설정 변경

--- src/main/java/lems/cowshed/config/aws/AwsS3Config.java
+++ src/main/java/lems/cowshed/config/aws/AwsS3Config.java
@@ -1,10 +1,10 @@
 package lems.cowshed.config.aws;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
@@ -13,8 +13,9 @@ public class AwsS3Config {
     private String region;
 
     @Bean
-    public AmazonS3 s3Client() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region).build();
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .build();
     }
 }
--- src/main/java/lems/cowshed/config/aws/AwsS3Util.java
+++ src/main/java/lems/cowshed/config/aws/AwsS3Util.java
@@ -1,13 +1,19 @@
 package lems.cowshed.config.aws;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import lems.cowshed.domain.UploadFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -15,40 +21,51 @@ import java.util.UUID;
 @Component
 public class AwsS3Util {
     @Value("${spring.application.bucket.name}")
-    private String bucketName;
-    private final AmazonS3 s3Client;
+    private String bucket;
+    private final S3Client s3Client;
 
-    public AwsS3Util(AmazonS3 s3Client) {
+    public AwsS3Util(S3Client s3Client) {
         this.s3Client = s3Client;
     }
 
     public List<UploadFile> uploadFiles(List<MultipartFile> multipartFiles) throws IOException {
-        if(multipartFiles.isEmpty()){
-            return null;
+        if (multipartFiles.isEmpty()) {
+            return List.of();
         }
 
         List<UploadFile> uploadFileList = new ArrayList<>();
-        for(MultipartFile multipartFile : multipartFiles){
+        for (MultipartFile multipartFile : multipartFiles) {
             uploadFileList.add(uploadFile(multipartFile));
         }
         return uploadFileList;
     }
 
-    public UploadFile uploadFile(MultipartFile multipartFile) throws IOException {
-        if(multipartFile == null || multipartFile.isEmpty()){
+    public UploadFile uploadFile(MultipartFile file) throws IOException {
+        if (file == null || file.isEmpty()) {
             return null;
         }
 
-        String originalFilename = multipartFile.getOriginalFilename();
+        String originalFilename = file.getOriginalFilename();
         String storeFileName = crateStoreFileName(originalFilename);
+        String ext = extractExt(originalFilename);
+        String route = "images/" + storeFileName + "." + ext;
 
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(multipartFile.getContentType());
-        objectMetadata.setContentLength(multipartFile.getInputStream().available());
-        s3Client.putObject(bucketName, storeFileName, multipartFile.getInputStream(), objectMetadata);
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(route)
+                .contentType(file.getContentType())
+                .build();
 
-        String accessUrl = s3Client.getUrl(bucketName, storeFileName).toString();
-        return new UploadFile(originalFilename,storeFileName, accessUrl);
+        s3Client.putObject(
+                request,
+                RequestBody.fromInputStream(
+                        file.getInputStream(),
+                        file.getSize()
+                )
+        );
+
+        String accessUrl = getPublicUrl(route);
+        return new UploadFile(originalFilename, storeFileName, accessUrl);
     }
 
     private String crateStoreFileName(String originalFilename) {
@@ -60,6 +77,24 @@ public class AwsS3Util {
     private String extractExt(String originalFilename) {
         int pos = originalFilename.lastIndexOf(".");
         return originalFilename.substring(pos + 1);
+    }
+
+    private String getPublicUrl(String key) {
+        S3Presigner preSigner = S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        PresignedGetObjectRequest preSignedRequest =
+                preSigner.presignGetObject(b -> b
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .getObjectRequest(g -> g
+                                .bucket(bucket)
+                                .key(key)
+                        )
+                );
+
+        return preSignedRequest.url().toString();
     }
 
 }
