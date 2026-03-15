f3d74b420bb0bfdb399d653d7d0311bc6ce05703
johuije <mikm7243@naver.com>
2026-03-15 17:23:15 +0900

    refactor: 정기모임 참여 낙관적 락 To 원자적 업데이트 변경

--- src/main/java/lems/cowshed/repository/regular/event/RegularEventRepository.java
+++ src/main/java/lems/cowshed/repository/regular/event/RegularEventRepository.java
@@ -1,24 +1,20 @@
 package lems.cowshed.repository.regular.event;
 
-import jakarta.persistence.LockModeType;
 import lems.cowshed.domain.regular.event.RegularEvent;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RegularEventRepository extends JpaRepository<RegularEvent, Long> {
 
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    Optional<RegularEvent> findWithOptimisticLockById(Long regularId);
-
-    @Query("select count(rep) from RegularEventParticipation rep join RegularEvent re on rep.regularEventId = re.id where re.id = :regularId")
-    long getParticipantCount(@Param("regularId") long regularId);
+    @Modifying
+    @Query("UPDATE RegularEvent r SET r.participantCount = r.participantCount + 1 WHERE r.id = :id")
+    int increaseParticipantCount(@Param("id") Long id);
 
     @Query("select rep.userId from RegularEventParticipation rep join RegularEvent re on rep.regularEventId = re.id where re.id = :regularId")
     List<Long> findParticipantsUserIdsByRegularId(@Param("regularId") Long regularId);
