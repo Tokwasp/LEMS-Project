78774eca5caf7229901ccc67d7c26ecf49ffcb68
johuije <mikm7243@naver.com>
2026-03-15 14:29:12 +0900

    test: 모임 참여 동시성 문제 테스트 및 disabled
    트랜잭션 미적용으로 인해 타 테스트에 영향을 끼치기 때문

--- src/test/java/lems/cowshed/service/event/EventParticipationFacadeTest.java
+++ src/test/java/lems/cowshed/service/event/EventParticipationFacadeTest.java
@@ -7,6 +7,7 @@ import lems.cowshed.global.exception.BusinessException;
 import lems.cowshed.repository.event.EventRepository;
 import lems.cowshed.repository.event.participation.EventParticipantRepository;
 import lems.cowshed.repository.user.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
@@ -36,16 +38,18 @@ class EventParticipationFacadeTest extends IntegrationTestSupport {
     @Autowired
     private UserRepository userRepository;
 
+    @Disabled
     @Test
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    @DisplayName("3명 정원인 모임에 3명이 동시 참여할 때 실패 시 재시도가 수행된다.")
+    @DisplayName("100명 정원인 모임에 1만명이 동시 참여할 때 실패 시 재시도가 수행된다.")
     void saveEventParticipation_ShouldRetry_WhenOptimisticLockExceptionOccurs() throws InterruptedException {
         //given
-        int taskCount = 3;
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        int taskCount = 10000;
+        int capacity = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
         CountDownLatch countDownLatch = new CountDownLatch(taskCount);
 
-        Event findEvent = eventRepository.save(createEvent("테스터", "테스트 모임", 3));
+        Event findEvent = eventRepository.save(createEvent("테스터", "테스트 모임", capacity));
 
         List<User> users = Stream
                 .generate(() -> {
@@ -56,11 +60,15 @@ class EventParticipationFacadeTest extends IntegrationTestSupport {
                 .limit(taskCount)
                 .toList();
 
+        AtomicInteger BusinessExceptionCount = new AtomicInteger(0);
+
         //when
         for (User user : users) {
             executorService.submit(() -> {
                 try {
                     eventParticipationFacade.saveEventParticipation(findEvent.getId(), user.getId());
+                } catch (Exception e) {
+                    BusinessExceptionCount.incrementAndGet();
                 } finally {
                     countDownLatch.countDown();
                 }
@@ -71,9 +79,10 @@ class EventParticipationFacadeTest extends IntegrationTestSupport {
 
         // then
         long participants = eventParticipantRepository.getParticipationCountById(findEvent.getId());
-        assertThat(participants).isEqualTo(3);
+        assertThat(participants).isEqualTo(100);
     }
 
+    @Disabled
     @Test
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @DisplayName("5명이 최대인원인 모임에 10명 동시 참여 시 재시도 후 Recover 메서드가 호출된다.")
